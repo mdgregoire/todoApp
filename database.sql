364f393722d7ab4312bcd7644ef17586b129d15f
Michael Gregoire <mdgregoire@gmail.com>
2018-02-18 00:02:44 -0600

    more cleanup

--- database.sql
+++ database.sql
@@ -38,4 +38,4 @@ VALUES ('Do Dishes', '03/17/18', 'N'),
 	   ('Do TPS Report', '02/17/18', 'N');
 
 INSERT INTO task_catergory (task_id, catergory_id)
-VALUES (1,1),(2,3),(3,1),(4,7),(5,2),(6,5),(7,6)
+VALUES (1,1),(2,3),(3,1),(4,7),(5,2),(6,5),(7,6);
