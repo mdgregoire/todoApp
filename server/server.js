6e72fde7fc193bd9896a309726764f9641d3c9fe
Michael Gregoire <mdgregoire@gmail.com>
2018-02-19 08:56:12 -0600

    heroku commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('server/public'));
